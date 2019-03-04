7be62f56beb671ae2ec8fb44527607245da3e738
Prateek Saxena <prtksxna@gmail.com>
2019-03-04 13:47:18 +0530

    fix(profile): header and typography

--- themes/wmfoundation/template-parts/header/profile-single.php
+++ themes/wmfoundation/template-parts/header/profile-single.php
@@ -15,16 +15,15 @@ $share_links  = ! empty( $profile_header_data['share_links'] ) ? $profile_header
 
 ?>
 
-<div class="header-main">
+<div class="header-main header-role">
 	<div class="header-content">
 		<h2 class="h4 uppercase eyebrow">
-			<a href="<?php echo esc_url( $back_to_link ); ?>">
-				<?php wmf_show_icon( 'arrow-back', 'icon-white material' ); ?>
+			<a class="back-arrow-link" href="<?php echo esc_url( $back_to_link ); ?>">
 				<?php echo esc_html( $staff_name ); ?>
 			</a>
 		</h2>
 
-		<h1 class="mar-bototm"><?php the_title(); ?></h1>
+		<h1><?php the_title(); ?></h1>
 
 		<div class="post-meta h4">
 			<span>
@@ -33,19 +32,18 @@ $share_links  = ! empty( $profile_header_data['share_links'] ) ? $profile_header
 					printf( '%1$s, %2$s', esc_html( $role ), esc_html( $team_name ) );
 				endif;
 				?>
-
 			</span>
 		</div>
 	</div>
 
 	<?php if ( ! empty( $share_links ) ) : ?>
-	<div class="rise-up">
+	<div class="rise-up side-list">
 		<?php
 		foreach ( $share_links as $link ) :
 
 			?>
-		<span class="link-list hover-highlight color-white uppercase mar-right">
-			<a href="<?php echo strpos( $link['link'], 'mailto' ) !== false ? esc_url( 'mailto:' . antispambot( str_replace( 'mailto:', '', $link['link'] ) ) ) : esc_url( $link['link'] ); ?>" class="color-white">
+		<span class="link-list mar-right">
+			<a href="<?php echo strpos( $link['link'], 'mailto' ) !== false ? esc_url( 'mailto:' . antispambot( str_replace( 'mailto:', '', $link['link'] ) ) ) : esc_url( $link['link'] ); ?>">
 				<?php echo esc_html( $link['title'] ); ?>
 			</a>
 		</span>
