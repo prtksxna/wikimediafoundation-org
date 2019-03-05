034831a79313abcbb19f434b46eeb7f436969c26
Prateek Saxena <prtksxna@gmail.com>
2019-03-05 06:25:45 +0530

    fix(blog): typography and layout

--- themes/wmfoundation/template-parts/header/page-single.php
+++ themes/wmfoundation/template-parts/header/page-single.php
@@ -15,7 +15,7 @@ $meta                 = ! empty( $page_header_data['page_meta'] ) ? $page_header
 ?>
 
 <div class="header-main">
-  <div class="header-content mar-bottom_lg">
+  <div class="header-content mar-bottom_lg header-single">
     <?php if ( ! empty( $h4_title ) ) : ?>
       <h2 class="h4 eyebrow">
         <?php if ( ! empty( $h4_link ) ) : ?>
@@ -31,7 +31,7 @@ $meta                 = ! empty( $page_header_data['page_meta'] ) ? $page_header
     <?php if ( ! empty( $title ) ) : ?>
       <h1><?php echo wp_kses( $title, array( 'span' => array( 'class' ) ) ); ?></h1>
     <?php endif; ?>
-    
+
     <?php if ( ! empty( $meta ) ) : ?>
       <div class="post-meta h4">
         <?php echo wp_kses( $meta, $allowed_tags ); ?>
